8df90bfc515b9371f38613e03d28cfd6f6510704
Gary Xue <gxue@actuate.com>
2005-05-18 00:54:42 +0000

    New ODA Dev branc 0517

--- plugins/org.eclipse.birt.data/src/org/eclipse/birt/data/engine/i18n/ResourceConstants.java
+++ plugins/org.eclipse.birt.data/src/org/eclipse/birt/data/engine/i18n/ResourceConstants.java
@@ -108,7 +108,7 @@ public class ResourceConstants
 	public final static String PARAMETER_POSITION_CANNOT_BE_LESS_THAN_ONE = "odaconsumer.ParameterPositionIsOneBased";
 	public final static String CANNOT_GET_PARAMETER_TYPE_NAME = "odaconsumer.CannotGetParameterTypeName";
 	public final static String CANNOT_GET_PARAMETER_MODE = "odaconsumer.CannotGetParameterMode";
-	public final static String CANOOT_GET_PARAMETER_SCALE = "odaconsumer.CannotGetParameterScale";
+	public final static String CANNOT_GET_PARAMETER_SCALE = "odaconsumer.CannotGetParameterScale";
 	public final static String CANNOT_GET_PARAMETER_PRECISION = "odaconsumer.CannotGetParameterPrecision";
 	public final static String CANNOT_GET_PARAMETER_ISNULLABLE = "odaconsumer.CannotGetParameterIsNullable";
 	public final static String SAME_PARAM_NAME_FOR_DIFFERENT_HINTS = "odaconsumer.SameParamNameForDifferentHints";
@@ -124,7 +124,7 @@ public class ResourceConstants
 	public final static String OUTPUT_PARAMETERS_UNSUPPORTED = "odaconsumer.OutputParameterUnsupported";
 	public final static String CANNOT_GET_CONNECTION_METADATA = "odaconsumer.CannotGetConnectionMetaData";
 	public final static String CANNOT_GET_MAX_CONNECTIONS = "odaconsumer.CannotGetMaxConnections";
-	public final static String CANNOT_GET_MAX_STATEMENTS = "odaconsumer.CannotGetMaxStatements";
+	public final static String CANNOT_GET_MAX_QUERIES = "odaconsumer.CannotGetMaxStatements";
 	public final static String CANNOT_GET_DS_METADATA = "odaconsumer.CannotGetDataSetMetaData";
 	public final static String CANNOT_PREPARE_STATEMENT = "odaconsumer.CannotPrepareStatement";
 	public final static String CANNOT_CLOSE_CONNECTION = "odaconsumer.CannotCloseConnection";
@@ -180,5 +180,6 @@ public class ResourceConstants
 	public final static String CANNOT_CLEAR_IN_PARAMETERS = "odaconsumer.CannotClearInParameters";
 	public final static String UNRECOGNIZED_ODA_TYPE = "odaconsumer.UnrecognizedOdaType";
 	public final static String CANNOT_GET_COLUMN_NATIVE_TYPE_NAME = "odaconsumer.CannotGetColumnNativeDataTypeName";
+	public final static String CANNOT_GET_PARAMETER_METADATA = "odaconsumer.CannotGetParameterMetaData";
 	
 }
